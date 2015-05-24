b326d88f5d3b1ebc4fbce822248dd1b59daf0e23
dicristomanuel <dicristomanuel@icloud.com>
2015-05-24 14:47:29 -0700

    Almost done with bubbles. Working on 4th one

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@
 	// ¯\_(ツ)_/¯ Hi! I'm Cody, I will help you navigate the code.
 
 
-if (document.cookie !== "ToDoList=bubbleFirst") {
-	bubbleFirst();
-} 
+// if (document.cookie !== "ToDoList=markFirstVisit") {
+// 	bubbleFirst();
+// 	var bubbleSecondShown = false;
+// 	var bubbleThirdShown = false;
+// 	var bubbleFourtShown = false;
+// } else {
+// 	var bubbleSecondShown = true;
+// 	var bubbleThirdShown = true;
+// 	var bubbleFourtShown = true;
+// }
+
+bubbleFirst();
 
 /* if bubbleFirst.cookie doesn't exist show first hint bubble. Also defining functions
 		for the other bubbles which will come later on  ¯\_(ツ) */ 
@@ -12,36 +21,84 @@ function bubbleFirst() {
 	var body = $("body");
 	var exit = false;
 		body.prepend($('<div class="hints">'));
-			$(".hints").prepend('<img src="img/xIcon.png" class="bubbles rotate" id="x">');
-			$(".hints").append('<img src="img/step1.png" alt="Step1 hints" class="bubbles float" id="firstBubble">');
+			$(".hints").prepend('<img src="img/xIcon.png" class="XL3 rotate" id="x">');
+			$(".hints").append('<img src="img/step1.png" alt="Step1 hints" class="XL3 L3 M3 S3 float" id="firstBubble">');
 			$("#x").click(function() {
 				exit = true;
 					if(exit === true) {
 						$(".hints").remove();
 					}
 				});
-			$("#firstBubble").click(function() {
+			$(".hints").click(function() {
+					$(this).remove();
+				})
+
+			$(".group-header").click(function() {
 					$(".hints").remove();
 				})
 }//bubbleFirst
 
+
+var bubbleSecondShown = false;
+var bubbleThirdShown = false;
+
 function bubbleSecond() {
 	var body = $("body");
 	var exit = false;
-		body.prepend($('<div class="hints">'));
-			$(".hints").prepend('<img src="img/xIcon.png" class="bubbles rotate" id="x">');
-			$(".hints").append('<img src="img/step2.png" alt="Step1 hints" class="bubbles float" id="secondBubble">');
+		body.prepend($('<div class="hints"></div>'));
+			$(".hints").prepend('<img src="img/xIcon.png" class="XL3"rotate" id="x">');
+			$(".hints").append('<img src="img/step2.png" alt="Step1 hints" class="float XL2 L2 M2 S2" id="secondBubble">');
 			$("#x").click(function() {
 				exit = true;
 					if(exit === true) {
 						$(".hints").remove();
 					}
 				});
+			
 			$("#secondBubble").click(function() {
 					$(".hints").remove();
 				})
-}//bubbleFirst
-	
+}//bubbleSecond
+
+
+function bubbleThird() {
+	var body = $("body");
+	var exit = false;
+		body.prepend($('<div class="hints"></div>'));
+			$(".hints").prepend('<img src="img/xIcon.png" class="XL3"rotate" id="x">');
+			$(".hints").append('<img src="img/step3.png" alt="Step1 hints" class="float XL4 L4 M4 S4" id="thirdBubble">');
+			$("#x").click(function() {
+				exit = true;
+					if(exit === true) {
+						$(".hints").remove();
+					}
+				});
+			
+			$("#thirdBubble").click(function() {
+					$(".hints").remove();
+				})
+}//bubbleSecond
+
+
+function bubbleFourth() {
+	var body = $("body");
+	var exit = false;
+		body.prepend($('<div class="hints"></div>'));
+			$(".hints").prepend('<img src="img/xIcon.png" class="XL3"rotate" id="x">');
+			$(".hints").append('<img src="img/step3.png" alt="Step1 hints" class="float XL4 L4 M4 S4" id="thirdBubble">');
+			$("#x").click(function() {
+				exit = true;
+					if(exit === true) {
+						$(".hints").remove();
+					}
+				});
+			
+			$("#thirdBubble").click(function() {
+					$(".hints").remove();
+				})
+}//bubbleSecond
+
+
 /* (ツ)_/¯ Selects toDoButton and when clicked appends the 
 						new item to the list */ 
 
@@ -70,9 +127,9 @@ var input = $(".group-header");
 	    					}, 600);
 			}//ifStatement
 
-			if (document.cookie !== "ToDoList=bubbleSecond") {
+			// if (document.cookie !== "ToDoList=bubbleSecond") {
 				bubbleSecond();
-			}//ifStatement  
+			// }//ifStatement  
 	});//click 
 
 
@@ -81,6 +138,12 @@ var input = $(".group-header");
 	$( input ).on('keypress', function(evt) {
     var key = evt.which;
 	    if (key === 13) {
+	    	
+	    	if(!bubbleSecondShown) {
+	    		bubbleSecond();
+	    		bubbleSecondShown = true;
+	    }//ifStatement
+
 		var current = $( "input" );
 		var value = $( current ).val();
 		var item = $('<li>' + value +'</li>');
@@ -102,20 +165,23 @@ var input = $(".group-header");
 	    					}, 600);
 			}//ifStatement
 		}//ifStatement
-
-		if (document.cookie !== "ToDoList=bubbleSecond") {
-				bubbleSecond();
-			}//ifStatement  
 	});
 
 
 
-/* (ツ)_/¯ Click and hold an item on the list in order to perform an action
+/* (ツ)_/¯ Click an item on the list in order to perform an action
 						like delete or heart it. */ 
 var item;
 var itemSelected = [];
 list.mousedown(function(evt) {
 	item = $(evt.srcElement);
+			$(".hints").remove();
+			
+			 	if(!bubbleThirdShown) {
+	    		bubbleThird();
+	    		bubbleThirdShown = true;
+	    }//ifStatement
+
     setTimeout(function() {
    		if(item.hasClass("makeBigger")) {
    			item.removeClass("makeBigger");
@@ -179,5 +245,5 @@ $("[data-type='trash']").hover(function() {
 		});
 
 
-document.cookie = "ToDoList=bubbleFirst";
-document.cookie = "ToDoList=bubbleSecond";
+// document.cookie = "ToDoList=markFirstVisit";
+
